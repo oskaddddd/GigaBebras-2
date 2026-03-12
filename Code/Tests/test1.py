3873e07832520905da0250269677f6187332dbf4
oskaddddd <oskaras.m@proton.me>
2026-03-12 23:32:45 +0200

    Trying to get the radio working

--- Code/Tests/test1.py
+++
@@ -1,6 +0,0 @@
-a = bytearray(0)
-b = bytearray(b'\xc2\x21\x3b')
-c = a+b
-print(c.)
-for x in c:
-    print(hex(x))
---
+++ Ground/test1.py
@@ -0,0 +1,5 @@
+from ground_station import receiver
+import logging 
+
+logging.getLogger().setLevel(logging.DEBUG)
+r = receiver()
