586808c2e65e05629d672aaaa1af8e5b6d731815
oskaddddd <oskaras.m@proton.me>
2026-05-03 21:59:49 +0300

    parashute wire length caculator

---
+++ Tests/wire_length.py
@@ -0,0 +1,14 @@
+side_length = 10
+h = 15
+wire_distance = 6
+
+tie_off_length = 1.5
+
+from math import sqrt
+r = (sqrt(2)+1)/2*side_length
+a = sqrt(((side_length-wire_distance)/2)**2 + r**2)
+
+b = sqrt((side_length/2)**2 + (r - wire_distance/2)**2)
+a_len = sqrt(a**2+h**2)
+b_len = sqrt(b**2 + h**2)
+print(f"wire 1:  {round(a_len, 3)}\nwire 2:  {round(b_len, 3)}\ndiff:  {round(a_len-b_len, 3)} \ntotal wire without tieoff:  {round(4*(a_len+b_len), 3)}\ntotal wire:  {round(4*(a_len+b_len+2*tie_off_length), 3)}")
